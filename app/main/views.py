2bc386ae5ff195eef3fb2331e609d111aaa80334
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-05 13:33:23 +0300

    add the update view

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,8 @@ from . import main
 from ..models import User, Review
 from ..requests import get_genre, get_genre_tracks, get_radio_tracks, get_chart, search_artist
 from flask import url_for, redirect,request,render_template,abort,flash
+from .. import db, photos
+from .forms import UpdateProfile
 
 
 @main.route('/')
@@ -64,3 +66,35 @@ def profile(uname):
         abort(404)
         
     return render_template("profile/profile.html", user=user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
