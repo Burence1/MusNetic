376be01fbcf6026313a92e2f8baa29718db576e4
Burens <53422530+Burence1@users.noreply.github.com>
2021-05-05 12:46:49 +0300

    Ft/tbl music (#8)
    * db-schema

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class DevConfig(Config):
   Config child class for development configurations
   '''
 
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cocoh:kitkAt10@localhost/music'
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://Burens:Hawaii@localhost/music'
 
   DEBUG=True
 
