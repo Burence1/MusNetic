5294ad763ae72cf5f4ae0e8144da67087c98a8fe
alvynah <alvynahw@gmail.com>
2021-05-06 13:55:49 +0300

    Style playlist and index page to completion

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $(".play").click(function() {
         $("#iframe1").show('1500');
     });
+    $(".playlistbtn").click(function() {
+        $("#myIframe2").show('1500');
+    });
 
 
 });
