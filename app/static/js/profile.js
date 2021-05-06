94652509b77392fa78f24561b93760eac82118de
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-07 01:18:57 +0300

    update the profile page

---
+++ app/static/js/profile.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    console.log("Content Loaded");
+    var tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
+    var tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
+
+    function showPanel(panelIndex, colorCode) {
+        tabButtons.forEach(function (node) {
+            node.style.backgroundColor = "";
+            node.style.color = "";
+        });
+        tabButtons[panelIndex].style.backgroundColor = colorCode;
+        tabButtons[panelIndex].style.color = 'white';
+        tabPanels.forEach(function (node) {
+            node.style.display = 'none';
+        });
+        tabPanels[panelIndex].style.display = "block";
+        tabPanels[panelIndex].style.backgroundColor = colorCode;
+    }
+    showPanel(0,"#FF6347");
+})
